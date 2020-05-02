85c3222e9a88227abba241165197897a365fca2c
vigriban <vigriban@tut.by>
2020-05-02 11:47:12 +0300

    Print attributes

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from PIL import Image
+
+
+def view_attributes(image):
+    width, height = image.size
+    format = image.mode
+    print(f'Ширина - {width}\nВысота - {height}\nЦветовая модель - {format}')
+
+
 if __name__ == '__main__':
     path = "/tmp/alupka.jpeg"
     image = Image.open(path)
-    print(image.size())
+    view_attributes(image)
